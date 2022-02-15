922d6264b9b610b2bb84bf0ff006f0da53bf502a
Martin Szabo <xszabom5@stuba.sk>
2022-02-15 12:48:18 +0100

    Moving object to mouse position

--- modules/classes.js
+++ modules/classes.js
@@ -4,16 +4,20 @@ const c = canvas.getContext('2d');
 // -------------------------------- CLASSES --------------------------------
 
 export class GameObject{
+
     constructor(color, position, dimensions){
         this.color = color;
+
         this.position = {
             x: position.x,
             y: position.y
         }
+
         this.dimensions = {
             width: dimensions.width,
             height: dimensions.height
         };
+
         this.drawTexture(color);
     }
 
@@ -36,32 +40,96 @@ export class GameObject{
 
     isColliding(collidingObject, offsetX, offsetY){
         // TODO implement offset for texture collision
-        return (!(this.x > collidingObject.x + collidingObject.width ||
-                this.x + this.width < collidingObject.x ||
-                this.y > collidingObject.y + collidingObject.height ||
-                this.y + this.height < collidingObject.y)
+        return (this.position.x > collidingObject.position.x + collidingObject.dimensions.width ||
+                this.position.x + this.dimensions.width < collidingObject.position.x ||
+                this.position.y > collidingObject.position.y + collidingObject.dimensions.height ||
+                this.position.y + this.dimensions.height < collidingObject.position.y
         )
     }
 }
 
 export class Unit extends GameObject{
-    constructor(texture, type, hp){
-        super(texture)
+    constructor(color, position, dimensions, type, hp){
+        super(color, position, dimensions);
         this.type = type;
         this.hp = hp;
     }
-
 }
 
-export class Peasent extends Unit{
+export class Character extends Unit{
+    constructor(color, position, dimensions, type, hp, bIsMoving){
+        super(color, position, dimensions, type, hp);
+        this.bIsMoving = bIsMoving || false;
+        this.posX = 0; this.posY = 0;
+    }
+
+    move(position, velocity){
+        if(this.getbIsMoving() === true){
+    
+            // Diffs
+            let dX = position.x - this.position.x;
+            let dY = position.y - this.position.y;
+
+            //TODO ked je dX a dY zaporne tak je pomalsi transition
+
+            if(dX < 0){
+                this.posX += (dX/velocity.x*2);
+            }
+            else{
+                this.posX += (dX/velocity.x);
+            }
+    
+            if(dY < 0){
+                this.posY += (dY/velocity.y*2);
+            }
+            else{
+                this.posY += (dY/velocity.y);
+            }
+    
+            // Smoothly transitioning between positions
+            
+            // Updating position
+            this.update(
+                {x: this.posX, y: this.posY},
+                {x:50, y:50},
+            );
+            
+            // Checking if we need to move in another frame
+            this.setbIsMoving(
+                this.isColliding({
+                    position:{
+                        x: position.x,
+                        y: position.y
+                    },
+                    dimensions:{
+                        width: 10,
+                        height: 10
+                    }
+                })
+            );
+
+            console.log("dX:" + Math.round(dX) + " dY:" + Math.round(dY));
+            //console.log("posX:" + Math.round(this.posX) + " posY:" + Math.round(this.posY));
+        }
+    }
+
+    setbIsMoving(bool){
+        // TODO Check type
+        this.bIsMoving = bool;
+    }
+
+    getbIsMoving(){ return this.bIsMoving; }
+}
+
+export class Peasent extends Character{
     build(){
         console.log("Building...");
     }
 }
 
-export class Guard extends Unit{
-    constructor(hp, armor){
-        super("Guard",hp);
+export class Guard extends Character{
+    constructor(color, position, dimensions, type, hp, bIsMoving, armor){
+        super(color, position, dimensions, "Guard", hp, bIsMoving);
         this.armor = armor;
     }
     attack(){
--- modules/functions.js
+++ modules/functions.js
@@ -2,9 +2,9 @@ import * as Classes from './classes.js';
 
 // -------------------------------- VARIABLES --------------------------------
 
-let then = Date.now(); 
-let now;
-let elapsed;
+
+let then = Date.now(), now, elapsed;
+
 
 const canvas = document.getElementById('board');
 const c = canvas.getContext('2d');
@@ -16,17 +16,29 @@ const mouse = {
     y: innerHeight / 2
 }
 
+
+
 // -------------------------------- EVENT LISTENERS --------------------------------
 
 addEventListener('click', (event) => {
     mouse.x = event.clientX;
     mouse.y = event.clientY;
+    obj.setbIsMoving(true);
 })
 
 // -------------------------------- FUNCTIONS --------------------------------
 
+
+function clearScreen() {
+    c.fillStyle = 'white';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+// Game Loop
 export function animate(fps){
     requestAnimationFrame(animate);
+    clearScreen();
+    
 
     // Calculating time elapsed since last loop
     now = Date.now();
@@ -36,15 +48,12 @@ export function animate(fps){
 
     if(elapsed > 1000/fps){ then = now - (elapsed % 1000/fps); }
 
-    obj.drawTexture(obj.color)
-    obj2.drawTexture(obj2.color)
 
-    obj.update({x:mouse.x, y:mouse.y}, {x:100, y:100});
-    const dist = Math.hypot(mouse.x - obj.position.x, mouse.y - obj.position.y);
-    
+    const velocity = {x:50, y:50}; //TODO set in object
+    obj.move(mouse, velocity);
+    obj.drawTexture(obj.color)
     // TODO smoothly update movment of rectangle from its origin to cursor location
     
 }
 
-let obj = new Classes.GameObject('black', {x:130, y:130},{width:150, height:150});
-let obj2 = new Classes.GameObject('red', {x:0, y:0},{width:150, height:150});
+let obj = new Classes.Character('black', {x:130, y:130}, {width:150, height:150}, 'rect', 100);
