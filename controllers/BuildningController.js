cacaaa62d377654dc164c1ffe2f3c53f7af3898c
Martin Szabo <martin.szabo@plasticomnium.com>
2022-05-02 19:44:41 +0200

    Implementing createUnit()

--- controllers/BuildningController.js
+++ controllers/BuildningController.js
@@ -1,14 +1,46 @@
 import * as Unit from "./UnitController.js"
+import * as GuardController from "./GuardController.js"
+import * as GuardModel from "../models/GuardModel.js"
+import * as View from "../views/View.js"
 
-export class Building extends Unit.UnitController {
-    constructor({model, view, faction}){
+export class BuildingController extends Unit.UnitController {
+    constructor({model, view}){
         super({
             model: model,
             view: view
         });
-        this.faction = faction;
     }
-    createUnit(){
-        console.log("Creating unit...");
+    createUnit({position, dimensions, hp, armor}, canvas, context) {
+        let color;
+
+        if(this.model.faction === 'player'){
+            color = 'green';
+        }
+        else if(this.model.faction === 'enemy'){
+            color = 'red';
+        }
+        else if(this.model.faction === 'neutral'){
+            color = 'gray';
+        }
+
+        let model = new GuardModel.GuardModel({
+            texture: color,
+            position: position,
+            dimensions: dimensions,
+            hp: hp,
+            armor: armor
+        });
+    
+        let view = new View.View({
+            model: model,
+            canvas: canvas,
+            context: context
+        });
+    
+        let controller = new GuardController.GuardController({
+            model: model,
+            view: view,
+        });
+        return {model, view, controller};
     }
 }
--- logic.js
+++ logic.js
@@ -9,6 +9,8 @@ let then = Date.now(), now, elapsed; // Used to calculate elapsed time (FPS)
 
 // TODO use arrays for multiple timeouts
 let timeoutID; // Store the timeout ID for later use
+let timeoutIDPlayer;
+let timeoutIDEnemy;
 
 const canvas = document.getElementById('board');
 const context = canvas.getContext('2d');
@@ -21,9 +23,11 @@ let playerController
 let enemyModel
 let enemyView
 let enemyController
-let buildingModel
-let buildingView
-let buildingController
+let buildingModels
+let buildingViews
+let buildingControllers
+let playerUnits = new Array();
+let enemyUnits = new Array();
 
 const mouse = {
     x: innerWidth / 2,
@@ -65,35 +69,92 @@ export function animate(fps, state){
         const velocity = {x:50, y:50}; //TODO set in object
         playerController.move(mouse, velocity);
     }
-    buildingController.view.drawTexture();
+
+    buildingControllers.forEach(buildingController => {
+        buildingController.view.drawTexture();
+    });
     enemyController.view.drawTexture();
-    playerController.view.drawTexture(); // Vykresli texturu
+    playerController.view.drawTexture();
     
+    playerUnits.forEach(playerUnit => {
+        playerUnit.view.drawTexture();
+    })
+
+    enemyUnits.forEach(enemyUnit => {
+        enemyUnit.view.drawTexture();
+    })
+    
+    if(playerController.model.hp <= 0) {
+        state.gameOver = true;
+        cancelAnimationFrame(frameID);
+    }
+
     if(playerController.isColliding(enemyController)){
         console.log("Colliding enemy");
         // Colliding enemy
     }
-
+    
     // TODO compact collision code to a function
-    if(playerController.isColliding(buildingController) && buildingController.model.faction != 'player'){
-        console.log("Coliding building");
-        
-        if(timeoutID === undefined){
-            timeoutID = setTimeout(() => {
-                if(playerController.isColliding(buildingController)){
-                    buildingController.model.color = playerController.model.color;
-                    buildingController.model.faction = 'player'
-                }
-                clearTimeout(timeoutID);
-                timeoutID = undefined;
-            }, 5000);    
-        }        
-    }
+    buildingControllers.forEach(buildingController => {
+        if(playerController.isColliding(buildingController) && buildingController.model.faction != 'player'){
+            console.log("Coliding building");
+            
+            if(timeoutID === undefined){
+                timeoutID = setTimeout(() => {
+                    if(playerController.isColliding(buildingController)){
+                        buildingController.model.color = playerController.model.color;
+                        buildingController.model.faction = 'player'
+                    }
+                    clearTimeout(timeoutID);
+                    timeoutID = undefined;
+                }, 5000);    
+            }        
+        }
+    
+        if(buildingController.model.faction === 'player'){
+            if(timeoutIDPlayer === undefined && playerUnits.length < 6){
+                timeoutIDPlayer = setTimeout(() => {
+                    playerUnits.push(buildingController.createUnit({
+                        position: {
+                            x: buildingController.model.position.x + Math.random() * 30,
+                            y: buildingController.model.position.y + Math.random() * 30
+                        },
+                        dimensions: {width: 50, height: 50},
+                        hp:100,
+                        armor: 100
+                    }, canvas, context));
+                    clearTimeout(timeoutIDPlayer);
+                    timeoutIDPlayer = undefined;
+                }, 5000); 
+            }
+        }
+        else if(buildingController.model.faction === 'enemy'){
+            if(timeoutIDEnemy === undefined && enemyUnits.length < 6){
+                timeoutIDEnemy = setTimeout(() => {
+                    enemyUnits.push(buildingController.createUnit({
+                        position: {
+                            x: buildingController.model.position.x + Math.random() * 30,
+                            y: buildingController.model.position.y + Math.random() * 30
+                        },
+                        dimensions: {width: 50, height: 50},
+                        hp:100,
+                        armor: 100
+                    }, canvas, context));
+                    clearTimeout(timeoutIDEnemy);
+                    timeoutIDEnemy = undefined;
+                }, 5000);
+            }
+        }
+    });
 
-    if(buildingController.model.faction === 'player'){
-        state.gameOver = true;
-        cancelAnimationFrame(frameID);
-    }
+    //TODO implementuj attack
+    //TODO implementuj vypocet vzdialenosti medzi dvoma objektami
+    //TODO vymysliet ako manipulovat hracove a enemakove units
+    //TODO prepojit zakladne, pridat nejaku premennu ktora bude odkazovat na dalsiu zakladnu ALEBO radius ktory by urcil tieto spojenia a urcil by kam sa treba pohnut
+
+    
+
+    
 }
 
 export function initialize(){
@@ -116,9 +177,11 @@ export function initialize(){
         view: playerView,
     });
 
+
+    // TODO treba toto pri incializacii, Nebolo by spravit iba buildingy?
     enemyModel = new Models.GuardModel.GuardModel({
         texture: 'red',
-        position: {x:50, y:30},
+        position: {x:500, y:600},
         dimensions: {width:50, height:50},
         hp: 100,
         armor: 100
@@ -135,24 +198,81 @@ export function initialize(){
         view: enemyView,
     });
     
-    buildingModel = new Models.BuildingModel.BuildingModel({
-        texture: 'grey',
-        position: {x:300, y:30},
-        dimensions: {width:50, height:50},
-        type: 'Building',
-        faction: 'neutral',
-        hp: 100,
-    });
+    buildingModels =[
+        new Models.BuildingModel.BuildingModel({
+            texture: 'red',
+            position: {x:600, y:600},
+            dimensions: {width:50, height:50},
+            type: 'Building',
+            faction: 'enemy',
+            hp: 100,
+        }),
+        new Models.BuildingModel.BuildingModel({
+            texture: 'grey',
+            position: {x:600, y:50},
+            dimensions: {width:50, height:50},
+            type: 'Building',
+            faction: 'neutral',
+            hp: 100,
+        }),
+        new Models.BuildingModel.BuildingModel({
+            texture: 'grey',
+            position: {x:350, y:350},
+            dimensions: {width:50, height:50},
+            type: 'Building',
+            faction: 'neutral',
+            hp: 100,
+        }),
+        new Models.BuildingModel.BuildingModel({
+            texture: 'grey',
+            position: {x:0, y:600},
+            dimensions: {width:50, height:50},
+            type: 'Building',
+            faction: 'neutral',
+            hp: 100,
+        })
+    ]
     
-    buildingView = new Views.View.View({
-        model: buildingModel,
-        canvas: canvas,
-        context: context
-    });
+    buildingViews = [
+        new Views.View.View({
+            model: buildingModels[0],
+            canvas: canvas,
+            context: context
+        }),
+        new Views.View.View({
+            model: buildingModels[1],
+            canvas: canvas,
+            context: context
+        }),
+        new Views.View.View({
+            model: buildingModels[2],
+            canvas: canvas,
+            context: context
+        }),
+        new Views.View.View({
+            model: buildingModels[3],
+            canvas: canvas,
+            context: context
+        }),
+    ]
     
-    buildingController = new Controllers.GuardController.GuardController({
-        model: buildingModel,
-        view: buildingView,
-    });
+    buildingControllers = [
+        new Controllers.BuildingController.BuildingController({
+            model: buildingModels[0],
+            view: buildingViews[0],
+        }),
+        new Controllers.BuildingController.BuildingController({
+            model: buildingModels[1],
+            view: buildingViews[1],
+        }),
+        new Controllers.BuildingController.BuildingController({
+            model: buildingModels[2],
+            view: buildingViews[2],
+        }),
+        new Controllers.BuildingController.BuildingController({
+            model: buildingModels[3],
+            view: buildingViews[3],
+        })
+    ]
 
 }
