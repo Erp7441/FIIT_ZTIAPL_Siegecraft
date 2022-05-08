16eb2ff628c81e359fb17ad26d8208249794cebb
Martin Szabo <xszabom5@stuba.sk>
2022-05-08 16:32:23 +0200

    Improved selection

--- views/View.js
+++ views/View.js
@@ -5,7 +5,10 @@ export class View{
         this.canvas = canvas;
     }
     drawTexture(){
-        if(this.model.faction === 'player'){
+        if(this.model.faction === 'player' && this.model.selected === true){
+            this.context.filter = "grayscale(100%) brightness(60%) sepia(100%) hue-rotate(+50deg) saturate(600%) contrast(0.8)";
+        }
+        else if(this.model.faction === 'player'){
             this.context.filter = "grayscale(100%) brightness(40%) sepia(100%) hue-rotate(+50deg) saturate(600%) contrast(0.8)";
         }
         else if(this.model.faction === 'enemy'){
